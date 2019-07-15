cb48c2567cc657bf7d880aebc41c311f6a7da787
Pikaso1 <52895508+Pikaso1@users.noreply.github.com>
2019-07-15 11:12:30 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,23 +1,21 @@
 jQuery(document).ready(function($) {
-
-$(".ajax-contact-form").submit(function() {
-var str = $(this).serialize();
-
-$.ajax({
-type: "POST",
-url: "/submit.php",
-data: str,
-success: function(msg) {
-if(msg == 'OK') {
-result = '<p>Your application is accepted</p>';
-$(".fields").hide();
-} else {
-result = msg;
-}
-$('.note').html(result);
-}
-});
-return false;
-});
+	$(".ajax-contact-form").submit(function() {
+		var str = $(this).serialize();
+		$.ajax({
+			type: "POST",
+			url: "/submit.php",
+			data: str,
+			success: function(msg) {
+				if (msg == 'OK') {
+					result = '<p>Your application is accepted</p>';
+					$(".fields").hide();
+				} else {
+					result = msg;
+				}
+				$('.note').html(result);
+			}
+		});
+		return false;
+	});
 });
 
